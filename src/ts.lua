09d0502bd09e8cc5c7830846e736e3f4d2d3bec8
Jonathan Holmes <Vorlias@users.noreply.github.com>
2019-08-19 16:53:49 +1200

    Update ts imports

--- src/ts.lua
+++ src/ts.lua
@@ -1,8 +1,13 @@
-local Roact = require(script.Parent.Parent.roact.roact.src)
-local createDragSource = require(script.createDragSource)(Roact)
-local createDropTarget = require(script.createDropTarget)(Roact)
+local Roact = require(script.Parent:FindFirstAncestor("node_modules"):WaitForChild("roact").roact.src)
+local parent = script.Parent
+local createDragSource = require(parent.createDragSource)(Roact)
+local createDropTarget = require(parent.createDropTarget)(Roact)
+local DragDropProvider = require(parent.DragDropProvider)(Roact)
+local DragDropContext = require(parent.DragDropContext)
 
 return {
-    DragFrame = createDragSource("Frame"),
-    DropFrame = createDropTarget("Frame")
+	DragFrame = createDragSource("Frame"),
+	DropFrame = createDropTarget("Frame"),
+	DragDropProvider = DragDropProvider,
+	DragDropContext = DragDropContext
 }

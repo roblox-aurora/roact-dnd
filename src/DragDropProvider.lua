afbfd9fbb3e411b2cc0a839a324c307b03be9446
Jonathan Holmes <pcf1235@aut.ac.nz>
2019-08-19 17:17:00 +1200

    Fixed DragDropProvider

--- src/DragDropProvider.lua
+++ src/DragDropProvider.lua
@@ -1,15 +1,17 @@
 return function(Roact)
-    local storeKey = require(script.Parent.storeKey)
-    local DragDropContext = require(script.Parent.DragDropContext)
+	local storeKey = require(script.Parent.storeKey)
+	local DragDropContext = require(script.Parent.DragDropContext)
 
-    local DragDropProvider = Roact.Component:extend("DragDropProvider")
-    function DragDropProvider:init(props)
-        local context = props.context or DragDropContext.Default
+	local DragDropProvider = Roact.Component:extend("DragDropProvider")
+	function DragDropProvider:init(props)
+		local context = props.context or DragDropContext.Default
 
-        self._context[storeKey] = context
-    end
+		self._context[storeKey] = context
+	end
 
-    function DragDropProvider:render()
-        return Roact.oneChild(self.props[Roact.Children])
-    end
+	function DragDropProvider:render()
+		return Roact.oneChild(self.props[Roact.Children])
+	end
+
+	return DragDropProvider
 end
